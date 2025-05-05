6bd473496294403c9b6c23b0a936a41bbe1df794
Chiu, Chih-Chia <66709644+Jwander0820@users.noreply.github.com>
2025-05-05 13:52:12 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,12 @@
+import os
 from flask import Flask
+
 app = Flask(__name__)
+
 @app.route("/")
-def keep_alive():
-    return "I'm alive!", 200
+def index():
+    return "Bot is alive!", 200
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # 如果沒有 PORT 就用 5000
+    app.run(host="0.0.0.0", port=port)
